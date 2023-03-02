d8763ef680702bcf7492a98f63c80b792506b209
andredcp <andre.dacostap3@gmail.com>
2023-03-02 03:38:57 +0100

    soon over adv

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,45 +1,52 @@
 import { useTranslation } from "react-i18next";
 import "../../i18n";
+import tslogo from "../../assets/ts.png";
+import jslogo from "../../assets/js.png";
+import nextlogo from "../../assets/nextjs.png";
+import reactlogo from "../../assets/reactjs.png";
+import nodelogo from "../../assets/nodejs.png";
+import gitlogo from "../../assets/git.png";
+import reactquerylogo from "../../assets/reactquery.png";
+import tailwindlogo from "../../assets/Tailwind.png";
+import postgrelogo from "../../assets/postgre.png";
+import mangodblogo from "../../assets/mangodb.png";
+import reduxlogo from "../../assets/redux.png";
 
 export function About() {
   const { t, i18n } = useTranslation();
   return (
     <div className="">
       <div className="font-Cairo">
-        <h2 className="text-4xl font-light">
-          J'ai 25 ans, je suis récemment devenu en décembre 2022 développeur web
-          fullstack, je suis née au Portugal, mais j'ai vécu presque toute ma
-          vie en France. Après de nombreuses expériences dans différents
-          domaines liés à l'informatique, j'ai décidé de me former au
-          developpement web via Ironhack. Après le Bootcamp Ironhack, j'ai fais
-          des projects personnels à fin de m'auto-former et être prêt pour le
-          marché du travail. Curieux et touche-à-tout lié au domaine WEB je suis
-          très enthousiaste de démarrer une nouvelle expérience.
+        <h2 className="text-3xl font-light">
+          J'ai 25 ans, née au Portugal, mais j'ai vécu presque toute ma vie en
+          France. Après de nombreuses expériences dans différents domaines liés
+          à l'informatique, j'ai décidé de me former au developpement web via
+          Ironhack. Après le Bootcamp Ironhack, j'ai fais des projects
+          personnels à fin de m'auto-former et être prêt pour le marché du
+          travail. Curieux et touche-à-tout lié au domaine WEB je suis très
+          enthousiaste de démarrer une nouvelle expérience.
         </h2>
+        <h2 className="text-2xl mt-10">Hobbies</h2>
+        <div className="w-full h-full rounded-3xl ">
+          <p className="text-xl font-light ">
+            Passionner par le Tennis & le Footbal. ReactJS, Typescript et ses
+            librairies, ainsi que NodeJS. Je commence également l'apprentissage
+            de NextJS.
+          </p>
+        </div>
         <div className="mt-20">
-          <h2 className="text-2xl">Diplômes</h2>
-          <div className=" w-full h-full rounded-3xl">
-            <p className="text-xl font-light">
-              GRETA LOGNES Técnico Assistência informática e redes, 2018 em
-              Lognes (França) <br />
-              IRONHACK Full-stack web developer, 2022 em remote (Portugal)
-            </p>
-          </div>
-          <h2 className="text-2xl mt-10">Skills</h2>
-          <div className="w-full h-full rounded-3xl ">
-            <p className="text-xl font-light ">
-              Passionner par le Tennis & le Footbal. ReactJS, Typescript et ses
-              librairies, ainsi que NodeJS. Je commence également
-              l'apprentissage de NextJS.
-            </p>
-          </div>
-          <h2 className="text-2xl mt-10">Hobbies</h2>
-          <div className="w-full h-full rounded-3xl ">
-            <p className="text-xl font-light ">
-              Passionner par le Tennis & le Footbal. ReactJS, Typescript et ses
-              librairies, ainsi que NodeJS. Je commence également
-              l'apprentissage de NextJS.
-            </p>
+          <h2 className="text-5xl mt-10">Skills</h2>
+          <div className="w-full h-full rounded-3xl flex mt-4">
+            <img className="h-32 w-32" src={tslogo} />
+            <img className="h-32 w-32" src={gitlogo} />
+            <img className="h-32 w-32" src={nextlogo} />
+            <img className="h-32 w-32" src={reactlogo} />
+            <img className="h-32 w-32" src={nodelogo} />
+            <img className="h-32 w-32" src={reactquerylogo} />
+            <img className="h-32 w-32" src={tailwindlogo} />
+            <img className="h-32 w-32" src={mangodblogo} />
+            <img className="h-32 w-32" src={postgrelogo} />
+            <img className="h-32 w-32" src={reduxlogo} />
           </div>
         </div>
       </div>
